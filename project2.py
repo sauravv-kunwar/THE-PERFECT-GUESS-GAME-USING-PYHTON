599f640dcd553df3bb887cf5c3b1031dd75e669b
Saurav Kunwar <saurabkunwar5@gmail.com>
2025-01-31 14:48:15 +0545

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,17 +1,20 @@
-
-
 import random
-n = random.randint(1,100)
 
+
+n = random.randint(1, 100)
 a = -1
-guesses = 1
-while(a != n):
-    a=int(input("Guess the number : "))
-    if(a > n):
-        print("Lower number please")
-    elif(a < n):
-        print("Higher number please")
-        guesses +=1
+guesses = 0
+
+
+while a != n:
     
-        
-print(f"You have guessed the number {n} correctly in {guesses} attempt ")       
+    a = int(input("Guess the number: "))
+    guesses += 1
+    
+    if a > n:
+        print("Lower number please")
+    elif a < n:
+        print("Higher number please")
+
+
+print(f"You have guessed the number {n} correctly in {guesses} attempts")
